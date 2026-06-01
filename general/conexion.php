ac17c2792d214fbb890ce42b5f75f3b84108fd61
FreizeitLeith <perezfrankrk@gmail.com>
2026-06-01 14:03:57 -0400

    actualizacion de conexion

--- general/conexion.php
+++ general/conexion.php
@@ -3,7 +3,7 @@ mysqli_report(MYSQLI_REPORT_OFF);
 
 $servidor = "sql104.infinityfree.com";
 $usuario = "if0_42011820";
-$password = "Pollocon12miel";   // <-- acá debe estar vacío, sin espacios
+$password = "Pollocon12miel";
 $base_datos = "if0_42011820_netstock_db";
 
 $conn = new mysqli($servidor, $usuario, $password, $base_datos);
