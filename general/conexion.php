3921251ef43043a49a2ef3e604e4b6d36cdf6844
FreizeitLeith <perezfrankrk@gmail.com>
2026-06-01 00:14:32 -0400

    actualizar registro, al fin funciona cono

--- general/conexion.php
+++ general/conexion.php
@@ -1,16 +1,13 @@
 <?php
-// Esta línea evita el Error 500 y nos obliga a mostrar el texto del error real
 mysqli_report(MYSQLI_REPORT_OFF);
 
 $servidor = "localhost";
 $usuario = "root";
-$password = "Pollocon12miel";
+$password = "";   // <-- acá debe estar vacío, sin espacios
 $base_datos = "if0_42011820_netstock_db";
 
-// Creamos la conexion
 $conn = new mysqli($servidor, $usuario, $password, $base_datos);
 
-// Verificamos la conexion
 if ($conn->connect_error) {
     die("🚨 ALERTA DE SISTEMA: " . $conn->connect_error);
 }
