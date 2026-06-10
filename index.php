fa99ce595de0608cf1091c4eeb6260ee139f1328
FreizeitLeith <perezfrankrk@gmail.com>
2026-06-09 22:00:59 -0400

    un poco de todo

--- index.php
+++ index.php
@@ -29,9 +29,8 @@
             <a href="login.php" class="btn-login" style="display:inline-block; font-size:1.1rem;">Acceder al Sistema</a>
         </div>
 
-       <div style="flex: 1; display: flex; gap: 20px; justify-content: flex-end; flex-wrap: wrap;">
-            <img src="img/dashboard-preview.png" alt="Vista previa de NetStock" style="max-width: 100%; width: 250px; border-radius: 12px; box-shadow: 0 20px 40px rgba(0,0,0,0.5); transform: perspective(1000px) rotateY(-10deg); transition: transform 0.5s ease;">
-            <img src="img/miFoto.png" alt="Mi foto personal" style="max-width: 100%; width: 250px; border-radius: 12px; box-shadow: 0 20px 40px rgba(0,0,0,0.5); transform: perspective(1000px) rotateY(-10deg); transition: transform 0.5s ease;">
+        <div style="flex: 1; text-align: right;">
+            <img src="img/dashboard-preview.png" alt="Vista previa de NetStock" style="max-width: 100%; width: 550px; border-radius: 12px; box-shadow: 0 20px 40px rgba(0,0,0,0.5); transform: perspective(1000px) rotateY(-10deg); transition: transform 0.5s ease;">
         </div>
 
     </section>
@@ -61,6 +60,11 @@
             <h3 style="margin-bottom: 5px; color: var(--primary-color);">FreizeitLeith</h3>
             <p style="color: var(--text-muted); margin-top:0;"><i class="fa-solid fa-code"></i> Ingeniería de Computación</p>
             <p>"La tecnología como herramienta para el orden."</p>
+        </div>
+
+        <div style="flex: 1; display: flex; gap: 20px; justify-content: flex-end; flex-wrap: wrap;">
+            <img src="img/dashboard-preview.png" alt="Vista previa de NetStock" style="max-width: 100%; width: 250px; border-radius: 12px; box-shadow: 0 20px 40px rgba(0,0,0,0.5); transform: perspective(1000px) rotateY(-10deg); transition: transform 0.5s ease;">
+             <img src="img/miFoto.png" alt="Mi foto personal" style="max-width: 100%; width: 250px; border-radius: 12px; box-shadow: 0 20px 40px rgba(0,0,0,0.5); transform: perspective(1000px) rotateY(-10deg); transition: transform 0.5s ease;">
         </div>
     </section>
 
